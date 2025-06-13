d2ea57c9912551550f4461d36ad1866f7a7daf6e
dongyoon7212 <dongyoon7212@naver.com>
2025-06-13 13:05:29 +0900

    Feat: 프론트 배포 시작

--- src/constants/serverAddress.js
+++ src/constants/serverAddress.js
@@ -1,6 +1,6 @@
 const LOCAL_ADDRESS = "http://localhost:8080";
-const DEPLOY_ADDRESS = "http://3.36.22.124";
+const DEPLOY_ADDRESS = "https://way-post-back.onrender.com";
 
 export default function getServerAddress() {
-	return LOCAL_ADDRESS;
+	return DEPLOY_ADDRESS;
 }
